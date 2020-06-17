69412c74b13a10bd5d473fce8a38620594ef59d1
Christian Knittl-Frank <lcnittl@gmail.com>
2020-06-17 13:25:30 +0200

    Port dmfo-diff to python

--- src/dmfo-diff/__main__.py
+++ src/dmfo-diff/__main__.py
@@ -13,8 +13,8 @@ import sys
 from pathlib import Path
 
 import colorlog
-
-# import pywin
+import pywintypes
+import win32com.client as win32
 
 LOG_LVLS = {
     # "NOTSET": logging.NOTSET,  # 0
@@ -24,6 +24,45 @@ LOG_LVLS = {
     "ERROR": logging.ERROR,  # 40
     "CRITICAL": logging.CRITICAL,  # 50
 }
+
+
+class WdSaveOptions:
+    # Prompt the user to save pending changes.
+    # wdPromptToSaveChanges = -2
+    # Save pending changes automatically without prompting the user.
+    # wdSaveChanges = -1
+    # Do not save pending changes.
+    wdDoNotSaveChanges = 0  # noqa: N815
+
+
+class WdUseFormattingFrom:
+    # Copy source formatting from the current item.
+    # wdFormattingFromCurrent = 0
+    # Copy source formatting from the current selection.
+    # wdFormattingFromSelected = 1
+    # Prompt the user for formatting to use.
+    wdFormattingFromPrompt = 2  # noqa: N815
+
+
+class WdWindowState:
+    # Normal.
+    # wdWindowStateNormal = 0
+    # Maximized.
+    wdWindowStateMaximize = 1  # noqa: N815
+    # Minimized.
+    wdWindowStateMinimize = 2  # noqa: N815
+
+
+class WdCompareDestination:
+    # Tracks the differences between the two files using tracked changes in the
+    # original document.
+    # wdCompareDestinationOriginal = 0
+    # Tracks the differences between the two files using tracked changes in the
+    # revised document.
+    wdCompareDestinationRevised = 1  # noqa: N815
+    # Creates a new file and tracks the differences between the original document
+    # and the revised document using tracked changes.
+    wdCompareDestinationNew = 2  # noqa: N815
 
 
 def parse_args() -> argparse.Namespace:
@@ -133,6 +172,139 @@ def setup_root_logger() -> logging.Logger:
     return logger
 
 
+def init_files() -> None:
+    for alias, FileName in FileNameMap.items():  # noqa: N806
+        # Write-Progress -Activity $activity -Status "Preparing $key" -PercentComplete $complete
+        FileName.resolve(strict=True)  # noqa: N806
+        FileNameMap[alias] = FileName
+        logging.debug("Processing '%s' ('%s')", alias, FileName)
+        logging.debug("Checking if is Git LFS pointer...")
+        cmd = f"git lfs pointer --check --file '{FileName}'"
+        ret = subprocess.run(  # nosec
+            shlex.split(cmd), stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr,
+        ).returncode
+        if ret == 0:
+            logging.debug("Is LFS pointer")
+            # is_lfs = True
+            logging.info("Converting LFS pointer to blob...")
+            AuxFileName = Path(str(FileName) + "_")  # noqa: N806
+            cmd = (
+                "cmd.exe /c 'type "
+                + str(FileName)
+                + " | git-lfs smudge > "
+                + str(AuxFileName)
+                + "'"
+            )
+            subprocess.run(  # nosec
+                shlex.split(cmd), stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr,
+            )
+            shutil.move(AuxFileName, FileName)
+            logging.debug("Done")
+        elif ret == 1:
+            logging.debug("Not LFS pointer")
+        elif ret == 2:
+            logging.critical("File not found")
+            sys.exit(1)
+        else:
+            logging.critical("Unknown return code '%s'!", ret)
+            sys.exit(1)
+
+        FileMode = FileName.stat().st_mode  # noqa: N806
+        logging.debug("File has %s mode", oct(FileMode))
+        if FileMode == 0o100444:
+            logging.debug("Removing read-only flag...")
+            FileName.chmod(0o666)
+            logging.debug("Done")
+        # $complete += 40
+    # $complete = 100
+    # Write-Progress -Activity $activity -Status "Done" -PercentComplete $complete
+    # sleep 1
+
+
+def dmfo_diff_wd() -> None:  # TODO: ?: -> int:
+    # . $PSScriptRoot\..\constants\const_wd.ps1
+
+    # $activity = "Compiling diff of '$DiffPath' with MS Word. This may take a while... "
+    # $complete = 0
+    # Write-Progress -Activity $activity -Status "Initializing COM object" -PercentComplete $complete
+    try:
+        logging.debug("Initializing COM object...")
+        COMObj = win32.gencache.EnsureDispatch("Word.Application")  # noqa: N806
+        COMObj.Visible = False
+        # $complete += 20
+        logging.debug("Done")
+    except pywintypes.com_error:
+        logging.critical(
+            "You must have Microsoft Word installed to perform this operation."
+        )
+        sys.exit(1)  # TODO: ?: return 1
+
+    try:
+        for alias, FileName in FileNameMap.items():  # noqa: N806
+            logging.debug("Processing '%s' ('%s')", alias, FileName)
+            # Write-Progress -Activity $activity -Status "Opening $key" -PercentComplete $complete
+            logging.debug("Opening '%s'", alias)
+            FileObjMap[alias] = COMObj.Documents.Open(  # noqa: N806
+                FileName=str(FileName),
+                ConfirmConversions=False,
+                ReadOnly=False,
+                AddToRecentFiles=False,
+            )
+            # $complete += 15
+            logging.debug("Done")
+
+        logging.debug("Diffing REMOTE vs LOCAL")
+        # Write-Progress -Activity $activity -Status "Diffing REMOTE vs LOCAL" -PercentComplete $complete
+        FileObjMap["DIFF"] = COMObj.CompareDocuments(
+            OriginalDocument=FileObjMap["LOCAL"],
+            RevisedDocument=FileObjMap["REMOTE"],
+            Destination=WdCompareDestination.wdCompareDestinationNew,
+            CompareFormatting=True,
+            CompareCaseChanges=True,
+            CompareWhitespace=True,
+            CompareTables=True,
+            CompareHeaders=True,
+            CompareFootnotes=True,
+            CompareTextboxes=True,
+            CompareFields=True,
+            CompareComments=True,
+            RevisedAuthor="REMOTE",
+            IgnoreAllComparisonWarnings=True,
+        )
+        # $complete += 10
+
+        for alias in ["LOCAL", "REMOTE"]:
+            logging.debug("Closing '%s'", alias)
+            # Write-Progress -Activity $activity -Status "Closing $key" -PercentComplete $complete
+            FileObjMap[alias].Close(SaveChanges=WdSaveOptions.wdDoNotSaveChanges)
+            FileObjMap.pop(alias)
+            # $complete += 5
+
+        logging.debug("Setting 'DIFF' to unsaved")
+        # Write-Progress -Activity $activity -Status "Setting DIFF to unsaved" -PercentComplete $complete
+        FileObjMap["DIFF"].Saved = 1
+        # $complete += 10
+
+        logging.debug("Bringing to foreground")
+        # Write-Progress -Activity $activity -Status "Bringing to foreground" -PercentComplete $complete
+        COMObj.Visible = True
+        COMObj.Activate()
+        COMObj.WindowState = WdWindowState.wdWindowStateMinimize
+        COMObj.WindowState = WdWindowState.wdWindowStateMaximize
+        # $complete = 100
+
+        # Write-Progress -Activity $activity -Status "Done" -PercentComplete $complete
+        # sleep 1
+    except pywintypes.com_error as exc:
+        logging.error("COM-Critical: '%s'", exc.args[1])
+        sys.exit(1)  # TODO: ?: return 1
+    sys.exit(0)  # TODO: ?: return 0
+
+
+def dmfo_diff_pp() -> None:
+    pass
+
+
 args = parse_args()
 root_logger = setup_root_logger()
 
@@ -147,64 +319,21 @@ FileNameMap = {
     "LOCAL": args.LocalFileName,
     "REMOTE": args.RemoteFileName,
 }
-FileMap = {}
+FileObjMap = {}
 # $complete += 20
 
+init_files()
 
-for alias, FileName in FileNameMap.items():
-    # Write-Progress -Activity $activity -Status "Preparing $key" -PercentComplete $complete
-    FileName.resolve(strict=True)
-    FileNameMap[alias] = FileName
-    logging.debug("Processing '%s'", FileName)
-    logging.debug("Checking if is Git LFS pointer...")
-    cmd = f"git lfs pointer --check --file '{FileName}'"
-    ret = subprocess.run(  # nosec
-        shlex.split(cmd), stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr,
-    ).returncode
-    if ret == 0:
-        logging.debug("Is LFS pointer")
-        is_lfs = True
-        logging.info("Converting LFS pointer to blob...")
-        AuxFileName = Path(str(FileName) + "_")
-        cmd = (
-            "cmd.exe /c 'type "
-            + str(FileName)
-            + " | git-lfs smudge > "
-            + str(AuxFileName)
-            + "'"
-        )
-        subprocess.run(  # nosec
-            shlex.split(cmd), stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr,
-        )
-        shutil.move(AuxFileName, FileName)
-        logging.debug("Done.")
-    elif ret == 1:
-        logging.debug("Not LFS pointer")
-    elif ret == 2:
-        logging.critical("File not found")
-        sys.exit(1)
-    else:
-        logging.critical("Unknown return code '%s'!", ret)
-        sys.exit(1)
-
-    FileMode = FileName.stat().st_mode
-    logging.debug("File has %s mode", oct(FileMode))
-    if FileMode == 0o100444:
-        logging.debug("Removing read-only flag...")
-        FileName.chmod(0o666)
-        logging.debug("Done.")
-    # $complete += 40
-# $complete = 100
-# Write-Progress -Activity $activity -Status "Done" -PercentComplete $complete
-# sleep 1
 
 if extension in [".doc", ".docx"]:
+    dmfo_diff_wd()  # TODO: ?: ret = dmfo_diff_wd()
     # . $PSScriptRoot\dmfo-diff\dmfo-diff_wd.ps1
     pass
 elif extension in [".ppt", ".pptx"]:
+    dmfo_diff_pp()  # TODO: ?: ret = dmfo_diff_pp()
     # . $PSScriptRoot\dmfo-diff\dmfo-diff_pp.ps1
     pass
 else:
     logging.critical("DMFO-Diff does not know what to do with '%s' files.", extension)
     sys.exit(1)
-# exit($LastExitCode)
+# exit(ret)
