89fd5c4c237433a4fe27ad521f96fd4860141d2e
Christian Knittl-Frank <lcnittl@gmail.com>
2020-07-21 16:19:42 +0200

    Fix wrong config path for ps1 scripts

--- ps1/reg_dmfo_drivers.ps1
+++ ps1/reg_dmfo_drivers.ps1
@@ -11,10 +11,10 @@ $InstallPath = $PSScriptRoot -replace "\\","/"
 
 # Register Diff Driver
 git config $scope diff.dmfo.name "DMFO diff driver"
-git config $scope diff.dmfo.command "powershell.exe -File $InstallPath/ps1/dmfo-diff.ps1"
+git config $scope diff.dmfo.command "powershell.exe -File $InstallPath/dmfo-diff.ps1"
 git config $scope diff.dmfo.binary "true"
 
 # Register Merge Driver
 git config $scope merge.dmfo.name "DMFO merge driver"
-git config $scope merge.dmfo.driver "powershell.exe -File $InstallPath/ps1/dmfo-merge.ps1 %O %A %B %L %P"
+git config $scope merge.dmfo.driver "powershell.exe -File $InstallPath/dmfo-merge.ps1 %O %A %B %L %P"
 git config $scope merge.dmfo.binary "true"
